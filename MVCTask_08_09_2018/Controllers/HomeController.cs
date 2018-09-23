e5f2f0bdfd4c12ae763520f2e0ec3d79318447c0
Kamil <ushurbakiev.k@gmail.com>
2018-09-23 12:14:28 +0600

    AddDbContext

--- MVCTask_08_09_2018/Controllers/HomeController.cs
+++ MVCTask_08_09_2018/Controllers/HomeController.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCTask_08_09_2018.Models;
 
 namespace MVCTask_08_09_2018.Controllers
 {
     public class HomeController : Controller
     {
+        private ArtsoftDb _context;
+
+        public HomeController()
+        {
+            _context = new ArtsoftDb();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ActionResult Index()
         {
             return View();
