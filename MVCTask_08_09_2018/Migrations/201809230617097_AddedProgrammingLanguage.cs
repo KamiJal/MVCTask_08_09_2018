4a30e97041de04a7fe5f0a03621608aa2bb2abd3
Kamil <ushurbakiev.k@gmail.com>
2018-09-23 12:20:45 +0600

    AddedMigration_AddedProgrammingLanguage

---
+++ MVCTask_08_09_2018/Migrations/201809230617097_AddedProgrammingLanguage.cs
@@ -0,0 +1,26 @@
+namespace MVCTask_08_09_2018.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedProgrammingLanguage : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ProgrammingLanguages",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 255),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.ProgrammingLanguages");
+        }
+    }
+}
