8105dd1141c4b8df025ea3afdb591a1e44aff3c7
Kamil <ushurbakiev.k@gmail.com>
2018-09-23 13:44:50 +0600

    AddedModel_Employee

--- MVCTask_08_09_2018/Models/Employee.cs
+++ MVCTask_08_09_2018/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,23 @@ namespace MVCTask_08_09_2018.Models
 {
     public class Employee
     {
+        [Required]
+        [StringLength(255)]
         public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string LastName { get; set; }
+
         public int Age { get; set; }
 
+        public int SexId { get; set; }
+        public Sex Sex { get; set; }
+
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+
+        public int ProgrammingLanguageId { get; set; }
+        public ProgrammingLanguage ProgrammingLanguage { get; set; }
     }
 }
