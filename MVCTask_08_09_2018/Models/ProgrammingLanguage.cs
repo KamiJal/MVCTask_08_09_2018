33d8fe778199e421f62bc593d78fcbfdb20dc6e8
Kamil <ushurbakiev.k@gmail.com>
2018-09-23 12:11:38 +0600

    AddedProgrammingLanguage

---
+++ MVCTask_08_09_2018/Models/ProgrammingLanguage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCTask_08_09_2018.Models
+{
+    public class ProgrammingLanguage
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
