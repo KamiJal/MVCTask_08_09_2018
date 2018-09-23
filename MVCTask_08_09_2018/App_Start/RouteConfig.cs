b105591f649e2b1eec094929bb7b09e261f5248d
Kamil <ushurbakiev.k@gmail.com>
2018-09-23 13:55:57 +0600

    ChangedLayout&DefaultRoute

--- MVCTask_08_09_2018/App_Start/RouteConfig.cs
+++ MVCTask_08_09_2018/App_Start/RouteConfig.cs
@@ -15,7 +15,7 @@ namespace MVCTask_08_09_2018
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
+                url: "{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
